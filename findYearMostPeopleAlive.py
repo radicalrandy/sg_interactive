85f0bc35cd11643d1b41e39cb794ea4a851f4a8c
radicalrandy <radicalrandy@gmail.com>
2015-04-15 14:10:44 -0500

    Adding global values

--- findYearMostPeopleAlive.py
+++ findYearMostPeopleAlive.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python
 
 import os, sys
+
+MinBirthEndYear = 1900 #Minimum Birthdate or Minimum End of life Year
+MaxBirthEndYear = 2000 #Maximum Birthdate or Maximum End of life Year
+yearWithNumberPeopleAlive = {} #Dictionary to hold values of number of people alive
